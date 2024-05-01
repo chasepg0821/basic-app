1d8b007bbce516e0619d13d82235f428297bace3
chasepg0821 <chasepg0821@gmail.com>
2024-05-01 12:39:12 -0400

    home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import { useAuthenticator } from '@aws-amplify/ui-react';
+
+const Home = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+
+  return (
+    <>
+      <h2>Welcome, {user.username}!</h2>
+      <button onClick={signOut}>Sign Out</button>
+    </>
+  );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Authenticator, View } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'; // default theme
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Authenticator.Provider>
+      <View><App /></View>
+    </Authenticator.Provider>
   </React.StrictMode>
 );
