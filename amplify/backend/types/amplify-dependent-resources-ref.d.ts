d2fc7310c324ff827981f2dc736f0f429940d33f
chasepg0821 <chasepg0821@gmail.com>
2024-05-13 12:48:33 -0400

    admin actions

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,5 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
+    "AdminQueries": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    },
     "basicappapi": {
       "ApiId": "string",
       "ApiName": "string",
@@ -19,6 +24,13 @@ export type AmplifyDependentResourcesAttributes = {
     }
   },
   "function": {
+    "AdminQueries1aca73df": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "Team00basicappapi": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
