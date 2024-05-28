ba48af1808a0336123c4b08a2e4ca502970c50b3
Chase Guynup <chasepg0821@gmail.com>
2024-05-28 13:47:21 -0400

    backend established

--- rdsConfig/schema.sql
+++ rdsConfig/schema.sql
@@ -1,14 +1,18 @@
+CREATE DATABASE `Blog`;
+
+USE `Blog`;
+
 CREATE TABLE `GlobalTypes` (
   `typeID` int(16) auto_increment,
   `acctType` varchar(15),
-  PRIMARY KEY (`typeID`)
+  PRIMARY KEY (`typeID`)GlobalTypes
 );
 
-CREATE TABLE `User` (
+CREATE TABLE `Users` (
   `userID` varchar(36),
   `globalType` int(16),
   PRIMARY KEY (`userID`),
-  FOREIGN KEY (`globalType`) REFERENCES `globalTypes`(`typeID`)
+  FOREIGN KEY (`globalType`) REFERENCES `GlobalTypes`(`typeID`)
 );
 
 CREATE TABLE `GroupTypes` (
@@ -17,7 +21,7 @@ CREATE TABLE `GroupTypes` (
   PRIMARY KEY (`typeID`)
 );
 
-CREATE TABLE `Group` (
+CREATE TABLE `Groups` (
   `groupID` int(16) auto_increment,
   `groupType` int(16),
   `description` varchar(250),
@@ -39,7 +43,7 @@ CREATE TABLE `User_Group` (
   PRIMARY KEY (`userID`, `groupID`),
   FOREIGN KEY (`userID`) REFERENCES `User`(`userID`),
   FOREIGN KEY (`groupID`) REFERENCES `Group`(`groupID`),
-  FOREIGN KEY (`membershipType`) REFERENCES `MembershipTypes`(`statusType`)
+  FOREIGN KEY (`membershipType`) REFERENCES `MembershipTypes`(`typeID`)
 );
 
 CREATE TABLE `Posts` (
@@ -52,4 +56,20 @@ CREATE TABLE `Posts` (
   FOREIGN KEY (`groupID`) REFERENCES `Group`(`groupID`),
   FOREIGN KEY (`userID`) REFERENCES `User`(`userID`)
 );
+
+INSERT INTO `GlobalTypes` (acctType) VALUES ('ADMIN'), ('MODERATOR'), ('USER');
+SELECT * FROM `GlobalTypes`;
+
+INSERT INTO `GroupTypes` (groupType) VALUES ('PUBLIC'), ('PRIVATE');
+SELECT * FROM `GroupTypes`;
+
+INSERT INTO `MembershipTypes` (statusType) VALUES ('ADMIN'), ('MODERATOR'), ('MEMBER'), ('PENDING'), ('REJECTED');
+SELECT * FROM `MembershipTypes`;
+
+INSERT INTO `Groups` (`groupType`, `description`, `name`) VALUES (1, 'This is a test group that is public!', 'Test Public Group'), (2, 'This is a test group that is private!', 'Test Private Group'); 
+SELECT `Groups`.groupID, `Groups`.`description`, `Groups`.name, `Groups`.groupType AS groupTypeID, GroupTypes.groupType
+	FROM `Groups`
+	INNER JOIN `GroupTypes` ON `Groups`.groupType=GroupTypes.typeID;
+
+
 
